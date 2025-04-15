cb3ff1f151de8d93e5fce9d7de25b4a4155abe0c
Amr <amruser97@gmail.com>
2025-04-16 01:37:11 +0200

    metadata

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,11 +6,12 @@ export default defineNuxtConfig({
   // App configuration
   app: {
     head: {
-      title: 'Appointment Scheduler',
+      title: 'Ehjzni',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/full.png' }],
     },
   },
 
